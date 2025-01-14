db62a2ce43823ce437019a066dc6c0e4829de70a
Mirek Simek <miroslav.simek@gmail.com>
2025-01-14 09:34:37 +0100

    gh action

--- develop.sh
+++ develop.sh
@@ -1,1 +1,1 @@
-docker run --rm --mount type=bind,src=.,dst=/nextra -p 3000:3000 sha256:3f43f4c8e520a8aee06ac06d2febcb7f57e83437ca751818bc78144bf61e0b1b
+docker run -it --rm --mount type=bind,src=.,dst=/nextra -p 3000:3000 sha256:3f43f4c8e520a8aee06ac06d2febcb7f57e83437ca751818bc78144bf61e0b1b
