419a496273527e583ca3aa384176e4dd69a92297
Lukas Hejtmanek <xhejtman@gmail.com>
2025-01-16 15:33:42 +0100

    add dummy cz pages

---
+++ pages/cz/_meta.js
@@ -0,0 +1,19 @@
+export default {
+  "index": {
+    "type": "page",
+    "title": "NRP Documentation",
+    "display": "hidden"
+  },
+  "intro-to-nrp": {
+    "title": "Introduction",
+    "type": "page"
+  },
+  "end_users": {
+    "title": "For End Users",
+    "type": "page"
+  },
+  "repo_admins": {
+    "title": "For Repository Administrators",
+    "type": "page"
+  }
+};
