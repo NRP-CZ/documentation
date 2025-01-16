101de44327692d309202e082075eb7ac7833b671
Lukas Hejtmanek <xhejtman@gmail.com>
2025-01-16 15:35:39 +0100

    fix index

--- pages/cz/_meta.js
+++ pages/cz/_meta.js
@@ -3,17 +3,5 @@ export default {
     "type": "page",
     "title": "NRP Documentation",
     "display": "hidden"
-  },
-  "intro-to-nrp": {
-    "title": "Introduction",
-    "type": "page"
-  },
-  "end_users": {
-    "title": "For End Users",
-    "type": "page"
-  },
-  "repo_admins": {
-    "title": "For Repository Administrators",
-    "type": "page"
   }
 };
