6327d7737dff1f9c063e578823dd1909839d6849
Lukas Hejtmanek <xhejtman@gmail.com>
2025-04-03 20:10:41 +0200

    test

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Banner from '@/public/img/publicity/ENG_eu-msmt-eosc-loga-barevne-ochrana
 export function Footer() {
    return (
      <footer className="mt-auto border-t bg-fd-secondary py-12 text-fd-secondary-foreground">
-     <div className="container flex flex-col gap-4 sm:flex-row sm:items-end sm:justify-between bg-fd-secondary z-20">
+     <div className="container flex flex-col gap-4 sm:flex-row sm:items-end sm:justify-between bg-fd-secondary relative z-20">
         <Image src={Banner} alt="publicity banner"/>
      </div>
      </footer>
