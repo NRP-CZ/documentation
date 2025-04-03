411a2d82ad04256f59b97a81d927ac95a1cef9ee
Lukas Hejtmanek <xhejtman@gmail.com>
2025-04-03 18:09:43 +0200

    fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,10 @@ import Banner from '@/public/img/publicity/ENG_eu-msmt-eosc-loga-barevne-ochrana
 
 export function Footer() {
    return (
+     <footer className="mt-auto border-t bg-fd-card py-12 text-fd-secondary-foreground">
      <div className="w-full flex justify-center">
         <Image src={Banner} alt="publicity banner"/>
      </div>
+     </footer>
    );
 }
