a7029924f3ae2b3ca847f6545602127c3571c102
Lukas Hejtmanek <xhejtman@gmail.com>
2025-04-03 18:04:47 +0200

    add component

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+import Banner from '@/public/img/publicity/ENG_eu-msmt-eosc-loga-barevne-ochrana-zona-paticka.svg';
+
+export function Footer() {
+   return (
+     <div className="w-full flex justify-center">
+        <Image src={Banner} alt="publicity banner"/>
+     </div>
+   );
+}
