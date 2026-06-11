964146dd2997c21ad10389d69f7850d0d1b54b6c
David Antos <david.antos@cesnet.cz>
2026-06-11 16:43:24 +0200

    semicolon for diversity

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,8 @@ export function Footer() {
    return (
      <footer className="mt-auto border-t py-12 text-fd-secondary-foreground ">
      <div className="container flex flex-col gap-4 sm:flex-row sm:items-end sm:justify-between --color-fd-background relative z-20">
-     <img src="https://webcentrum.muni.cz/media/3832168/seda_eu-msmt_eng.png" alt="EU and MŠMT Logos" style={{ height=60 }} />
-     <img src="https://webcentrum.muni.cz/media/3831863/seda_eosc.png" alt="EOSC CZ Logo" style={{ height=60 }} />
+     <img src="https://webcentrum.muni.cz/media/3832168/seda_eu-msmt_eng.png" alt="EU and MŠMT Logos" style={{ height:60 }} />
+     <img src="https://webcentrum.muni.cz/media/3831863/seda_eosc.png" alt="EOSC CZ Logo" style={{ height:60 }} />
      </div>
      </footer>
    );
